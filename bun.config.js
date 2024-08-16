902314719170f51d3f12e2a519e8823c3970206f
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2024-08-16 16:36:04 +0200

    udape

--- bun.config.js
+++ bun.config.js
@@ -1,9 +1,59 @@
-import pkg from "./package.json";
+import { build } from "bun";
+import * as path from "path";
+import * as fs from "fs";
+import pkg from "./module.json";
+import archiver from "archiver";
+import ModFS from "modfs";
+import { stringify, parse } from "ini";
 
-Bun.build({
-  entrypoints: ["./src/index.jsx"],
+function archiveFolder(sourceDir, outputFilePath) {
+  // Create a file to stream archive data to.
+  const output = fs.createWriteStream(outputFilePath);
+  const archive = archiver("zip", {
+    zlib: { level: 9 }, // Sets the compression level.
+  });
+
+  // Listen for all archive data to be written.
+  output.on("close", () => {
+    console.log(`${archive.pointer()} total bytes`);
+    console.log(
+      "Archiver has been finalized and the output file descriptor has closed."
+    );
+  });
+
+  // Good practice to catch warnings (e.g. stat failures) explicitly.
+  archive.on("warning", (err) => {
+    if (err.code !== "ENOENT") {
+      throw err;
+    }
+    console.warn(err);
+  });
+
+  // Good practice to catch this error explicitly.
+  archive.on("error", (err) => {
+    throw err;
+  });
+
+  // Pipe archive data to the file.
+  archive.pipe(output);
+
+  // Append files from sourceDir.
+  archive.directory(sourceDir, false);
+
+  // Finalize the archive (i.e. we are done appending files but streams have to finish yet).
+  archive.finalize();
+}
+
+const modfs = new ModFS(pkg.metadata);
+
+build({
+  entrypoints: [path.resolve(__dirname, pkg.build.modconf)],
   naming: "[dir]/[name].jsx",
-  outdir: "./dist",
+  outdir: path.resolve(
+    __dirname,
+    "magisk/system/usr/share/mmrl/config",
+    pkg.metadata.id
+  ),
   target: "browser",
   format: "esm",
   external: [
@@ -17,4 +67,12 @@ Bun.build({
   minify: true,
 }).then((res) => {
   console.log(res);
+  const prop = stringify(modfs.formatEntries());
+  fs.writeFileSync(
+    path.resolve(__dirname, "magisk/module.prop"),
+    prop,
+    "utf-8"
+  );
+  const output = `dist/${modfs.format(pkg.dist.moduleFileName)}`;
+  archiveFolder(path.resolve(__dirname, "magisk"), output);
 });
---
+++ magisk/install.sh
@@ -0,0 +1,20 @@
+SKIPMOUNT=false
+PROPFILE=false
+POSTFSDATA=false
+LATESTARTSERVICE=false
+
+print_modname() {
+    ui_print ""
+}
+
+on_install() {
+    ui_print "- Extracting module files"
+    unzip -qq -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+    [ -d "$MODPATH/system/bin/" ] || mkdir -p "$MODPATH/system/bin/"    
+}
+
+set_permissions() {
+    # The following is the default rule, DO NOT remove
+    set_perm_recursive $MODPATH 0 0 0755 0644
+    # set_perm $MODPATH/system/bin/YOUR_BIN 0 0 0755
+}
